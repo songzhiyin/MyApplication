c8a4f6739455f5d9b3302646f3deb9d1fe70b1e2
songzhiyin <916924575@qq.com>
2019-07-24 11:12:07 +0800

    视频的录制和播放

--- app/src/main/java/com/szy/myapplication/UI/Util/VideoPlayActivity.java
+++ app/src/main/java/com/szy/myapplication/UI/Util/VideoPlayActivity.java
@@ -25,7 +25,7 @@ import java.util.Calendar;
 /**
  * 视频的录制和播放
  */
-public class VideoPlayActivity extends BaseActivity implements SurfaceHolder.Callback{
+public class VideoPlayActivity extends BaseActivity implements SurfaceHolder.Callback {
     private static final String TAG = "MainActivity";
     private SurfaceView mSurfaceview;
     private Button mBtnStartStop;
@@ -34,7 +34,6 @@ public class VideoPlayActivity extends BaseActivity implements SurfaceHolder.Cal
     private boolean mIsPlay = false;//是否正在播放录像
     private MediaRecorder mRecorder;
     private SurfaceHolder mSurfaceHolder;
-    private ImageView mImageView;
     private Camera camera;
     private MediaPlayer mediaPlayer;
     private String path;
@@ -45,10 +44,11 @@ public class VideoPlayActivity extends BaseActivity implements SurfaceHolder.Cal
         @Override
         public void run() {
             text++;
-            textView.setText(text+"");
-            handler.postDelayed(this,1000);
+            textView.setText(text + "");
+            handler.postDelayed(this, 1000);
         }
     };
+
     @Override
     protected int getContentViewResId() {
         return R.layout.activity_video_play;
@@ -58,128 +58,139 @@ public class VideoPlayActivity extends BaseActivity implements SurfaceHolder.Cal
     protected void initViews() {
         super.initViews();
         mSurfaceview = (SurfaceView) findViewById(R.id.surfaceview);
-        mImageView = (ImageView) findViewById(R.id.imageview);
         mBtnStartStop = (Button) findViewById(R.id.btnStartStop);
         mBtnPlay = (Button) findViewById(R.id.btnPlayVideo);
-        textView = (TextView)findViewById(R.id.text);
-        mBtnStartStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mIsPlay) {
-                    if (mediaPlayer != null) {
-                        mIsPlay = false;
-                        mediaPlayer.stop();
-                        mediaPlayer.reset();
-                        mediaPlayer.release();
-                        mediaPlayer = null;
-                    }
-                }
-                if (!mStartedFlg) {
-                    handler.postDelayed(runnable,1000);
-                    mImageView.setVisibility(View.GONE);
-                    if (mRecorder == null) {
-                        mRecorder = new MediaRecorder();
-                    }
-
-                    camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
-                    if (camera != null) {
-                        camera.setDisplayOrientation(90);
-                        camera.unlock();
-                        mRecorder.setCamera(camera);
-                    }
-
-                    try {
-                        // 这两项需要放在setOutputFormat之前
-                        mRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
-                        mRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-
-                        // Set output file format
-                        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-
-                        // 这两项需要放在setOutputFormat之后
-                        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-                        mRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
-
-                        mRecorder.setVideoSize(640, 480);
-                        mRecorder.setVideoFrameRate(30);
-                        mRecorder.setVideoEncodingBitRate(3 * 1024 * 1024);
-                        mRecorder.setOrientationHint(90);
-                        //设置记录会话的最大持续时间（毫秒）
-                        mRecorder.setMaxDuration(30 * 1000);
-                        mRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
-
-                        path = getSDPath();
-                        if (path != null) {
-                            File dir = new File(path + "/recordtest");
-                            if (!dir.exists()) {
-                                dir.mkdir();
-                            }
-                            path = dir + "/" + getDate() + ".mp4";
-                            mRecorder.setOutputFile(path);
-                            mRecorder.prepare();
-                            mRecorder.start();
-                            mStartedFlg = true;
-                            mBtnStartStop.setText("Stop");
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    //stop
-                    if (mStartedFlg) {
-                        try {
-                            handler.removeCallbacks(runnable);
-                            mRecorder.stop();
-                            mRecorder.reset();
-                            mRecorder.release();
-                            mRecorder = null;
-                            mBtnStartStop.setText("Start");
-                            if (camera != null) {
-                                camera.release();
-                                camera = null;
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    mStartedFlg = false;
-                }
-            }
-        });
-
-        mBtnPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mIsPlay = true;
-                mImageView.setVisibility(View.GONE);
-                if (mediaPlayer == null) {
-                    mediaPlayer = new MediaPlayer();
-                }
-                mediaPlayer.reset();
-                Uri uri = Uri.parse(path);
-                mediaPlayer = MediaPlayer.create(mContext, uri);
-                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                mediaPlayer.setDisplay(mSurfaceHolder);
-                try{
-                    mediaPlayer.prepare();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                mediaPlayer.start();
-            }
-        });
-
+        textView = (TextView) findViewById(R.id.text);
         SurfaceHolder holder = mSurfaceview.getHolder();
         holder.addCallback(this);
         // setType必须设置，要不出错.
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
+
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (!mStartedFlg) {
-            mImageView.setVisibility(View.VISIBLE);
+    protected void initEvents() {
+        super.initEvents();
+        mBtnPlay.setOnClickListener(onClickListener);
+        mBtnStartStop.setOnClickListener(onClickListener);
+    }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btnStartStop://录制视频
+                    recordVideo();
+                    break;
+                case R.id.btnPlayVideo://播放视频
+                    playVideo();
+                    break;
+            }
         }
+    };
+
+    /**
+     * 录制视频
+     */
+    private void recordVideo() {
+        if (mIsPlay) {//是否正在播放视频
+            if (mediaPlayer != null) {
+                mIsPlay = false;
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
+        }
+        if (!mStartedFlg) {//是否正在播放视频
+            handler.postDelayed(runnable, 1000);
+            if (mRecorder == null) {
+                mRecorder = new MediaRecorder();
+            }
+            camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+            if (camera != null) {
+                camera.setDisplayOrientation(90);//设置视频预览角度
+                camera.unlock();
+                mRecorder.setCamera(camera);
+            }
+
+            try {
+                // 这两项需要放在setOutputFormat之前
+                mRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);//设置音频来源
+                mRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);//设置视频来源
+
+                // Set output file format
+                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+
+                // 这两项需要放在setOutputFormat之后
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                mRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
+
+                mRecorder.setVideoSize(640, 480);//设置视频分辨率
+                mRecorder.setVideoFrameRate(30);//设置录制视频的帧率即1秒30帧
+                mRecorder.setVideoEncodingBitRate(3 * 1024 * 1024);//设置视频的大小
+                mRecorder.setOrientationHint(90);//设置视频的角度
+                //设置记录会话的最大持续时间（毫秒）
+                mRecorder.setMaxDuration(30 * 1000);
+                mRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
+
+                path = getSDPath();
+                if (path != null) {
+                    File dir = new File(path + "/recordtest");
+                    if (!dir.exists()) {
+                        dir.mkdir();
+                    }
+                    path = dir + "/" + getDate() + ".mp4";
+                    mRecorder.setOutputFile(path);
+                    mRecorder.prepare();
+                    mRecorder.start();
+                    mStartedFlg = true;
+                    mBtnStartStop.setText("Stop");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            //stop
+            if (mStartedFlg) {
+                try {
+                    handler.removeCallbacks(runnable);
+                    mRecorder.stop();
+                    mRecorder.reset();
+                    mRecorder.release();
+                    mRecorder = null;
+                    mBtnStartStop.setText("Start");
+                    if (camera != null) {
+                        camera.release();
+                        camera = null;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            mStartedFlg = false;
+        }
+    }
+
+
+    /**
+     * 播放视频
+     */
+    private void playVideo() {
+        mIsPlay = true;
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+        }
+        mediaPlayer.reset();
+        Uri uri = Uri.parse(path);
+        mediaPlayer = MediaPlayer.create(mContext, uri);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDisplay(mSurfaceHolder);
+        try {
+            mediaPlayer.prepare();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.start();
     }
 
     /**
@@ -244,7 +255,7 @@ public class VideoPlayActivity extends BaseActivity implements SurfaceHolder.Cal
             camera.release();
             camera = null;
         }
-        if (mediaPlayer != null){
+        if (mediaPlayer != null) {
             mediaPlayer.release();
             mediaPlayer = null;
         }
