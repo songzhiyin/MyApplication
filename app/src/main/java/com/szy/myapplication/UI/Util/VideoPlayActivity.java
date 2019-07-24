847ee14571fefb32aaf881c11551ac94dbaa062d
songzhiyin <916924575@qq.com>
2019-07-24 15:46:50 +0800

    视频的录制和播放

--- app/src/main/java/com/szy/myapplication/UI/Util/VideoPlayActivity.java
+++ app/src/main/java/com/szy/myapplication/UI/Util/VideoPlayActivity.java
@@ -2,28 +2,17 @@ package com.szy.myapplication.UI.Util;
 
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
-import android.media.AudioManager;
-import android.media.MediaCodecInfo;
-import android.media.MediaCodecList;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Environment;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.szy.myapplication.Base.BaseActivity;
 import com.szy.myapplication.R;
-import com.szy.myapplication.Utils.H264Encoder;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.Calendar;
 
 /**
  * 视频的录制和播放
@@ -33,11 +22,10 @@ public class VideoPlayActivity extends BaseActivity {
     private SurfaceView surfaceView;
     private SurfaceHolder holder;
     private Camera camera;
-    private H264Encoder encoder;
-    private int width = 1280;
-    private int height = 720;
-    private int framerate = 30; //一秒30帧
-    private boolean isRecordVideo = false;
+    private MediaRecorder mediaRecorder;
+    File avfile; //存储位置
+    //标志
+    private boolean isRecording = false;
 
 
     @Override
@@ -52,7 +40,7 @@ public class VideoPlayActivity extends BaseActivity {
         tvStopRecoreVideo = $(R.id.tvStopRecoreVideo);
         surfaceView = $(R.id.surfaceview);
         holder = surfaceView.getHolder();
-        camera = Camera.open(); //开启相机
+        camera = Camera.open(0); //开启相机
         camera.setDisplayOrientation(90);
     }
 
@@ -61,7 +49,7 @@ public class VideoPlayActivity extends BaseActivity {
         super.initEvents();
         tvStartRecoreVideo.setOnClickListener(onClickListener);
         tvStopRecoreVideo.setOnClickListener(onClickListener);
-        camera.setPreviewCallback(previewCallback);
+//        camera.setPreviewCallback(previewCallback);
         holder.addCallback(callback);
     }
 
@@ -70,74 +58,90 @@ public class VideoPlayActivity extends BaseActivity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.tvStartRecoreVideo://开始录制
-                    if (isRecordVideo == false && encoder != null) {
-                        isRecordVideo = true;
-                        encoder.startEncoder(); //开始编码
-                    }
+                    startRecorderVideo();
                     break;
                 case R.id.tvStopRecoreVideo://停止录制
-                    if (camera != null && isRecordVideo) {
-                        camera.setPreviewCallback(null);
-                        camera.stopPreview();
-                        camera = null;
-                        isRecordVideo = false;
-                    }
-                    if (encoder != null) {
-                        encoder.stopEncoder();
-                    }
+                    stopRecoreVideo();
                     break;
             }
         }
     };
 
-    /**
-     * 判断该设备是否支持H264编码
-     */
-    private boolean supportH264Codec() {
-        // 遍历支持的编码格式信息,并查询有没有支持H.264(avc)的编码
-        if (Build.VERSION.SDK_INT >= 18) {
-            //计算可用的编解码器数量
-            int number = MediaCodecList.getCodecCount();
-            for (int i = number - 1; i > 0; i--) {
-                //获得指定的编解码器信息
-                MediaCodecInfo codecInfo = MediaCodecList.getCodecInfoAt(i);
-                //得到支持的类型
-                String[] types = codecInfo.getSupportedTypes();
-                //查询有没有支持H.264(avc)的编码
-                for (int j = 0; j < types.length; j++) {
-                    if (types[j].equalsIgnoreCase("video/avc")) {
-                        return true;
-                    }
-                }
-            }
+    private void startRecorderVideo() {
+        //文件保存位置
+        avfile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "test.mp4");
+        if (avfile.exists()) {
+            avfile.delete();
         }
-        return false;
+        try {
+            avfile.createNewFile();
+        } catch (IOException e) {
+        }
+        mediaRecorder = new MediaRecorder();
+        //*******************************************
+        //预览时旋转90度正常
+        Camera.Parameters params = camera.getParameters();
+        //自动聚焦,不然的话可嫩不清晰
+        params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+        camera.setParameters(params);
+        //解锁相机允许另一个进程访问它
+        camera.unlock();
+        mediaRecorder.setCamera(camera);
+//********************************************
+        mediaRecorder.reset();
+        //设置声音来源 和 视频来源
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        //设置输出文件的格式,必须在设置编码格式之前设置
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        //设置编码格式 音频 和 视频 格式要设置正确，不然有时候录制黑屏
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        //设置分辨率,可以改变清晰度 这个值没设置对的话会崩溃  在编码格式之后设置 可以默认
+        mediaRecorder.setVideoSize(1920, 1080);
+        //设置帧率,每秒25帧 这个值没设置对的话会崩溃  在编码格式之后设置
+        mediaRecorder.setVideoFrameRate(30);
+        // 设置位率，不设置可能画质不清晰
+        mediaRecorder.setVideoEncodingBitRate(1920 * 1080 * 5);
+        // 输出旋转90度播放时，但是预览时没有旋转90度,预览由Camera决定
+        mediaRecorder.setOrientationHint(90);
+        //设置输出文件位置 注意这里不是传入一个文件，而是路径
+        mediaRecorder.setOutputFile(avfile.getAbsolutePath());
+        //设置预览
+        mediaRecorder.setPreviewDisplay(surfaceView.getHolder().getSurface());
+        //准备录制
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //开始录制
+        mediaRecorder.start();
+        //
+        Toast.makeText(this, "开始录制...", Toast.LENGTH_SHORT).show();
+        tvStartRecoreVideo.setEnabled(false);
+        tvStopRecoreVideo.setEnabled(true);
+        isRecording = true;
     }
 
-    private byte[] rotateYUV420Degree270(byte[] data, int imageWidth, int imageHeight){
-        byte[] yuv =new byte[imageWidth*imageHeight*3/2];
-        // Rotate the Y luma
-        int i =0;
-        for(int x = imageWidth-1;x >=0;x--){
-            for(int y =0;y < imageHeight;y++){
-                yuv[i]= data[y*imageWidth+x]; 		            i++;
-            }   }// Rotate the U and V color components  	i = imageWidth*imageHeight;
-        for(int x = imageWidth-1;x >0;x=x-2){
-            for(int y =0;y < imageHeight/2;y++){ 		       yuv[i]= data[(imageWidth*imageHeight)+(y*imageWidth)+(x-1)]; 		         i++; 		       yuv[i]= data[(imageWidth*imageHeight)+(y*imageWidth)+x]; 		            i++;
-            }
+    private void stopRecoreVideo() {
+        if (isRecording) {
+            //停止录制
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            //
+            tvStartRecoreVideo.setEnabled(true);
+            tvStopRecoreVideo.setEnabled(false);
         }
-        return yuv;
+        if (camera != null) {
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            camera = null;
+        }
+
     }
-    private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
-        @Override
-        public void onPreviewFrame(byte[] bytes, Camera camera) {
-//返回相机预览的视频数据,并给H264Encoder编码压缩为H.264(avc)的文件test.mp4
-            //这里面的Bytes的数据就是NV21格式的数据
-            if (encoder != null && isRecordVideo) {
-                encoder.putDate(rotateYUV420Degree270(bytes,1280,720)); //将一帧的数据传过去处理
-            }
-        }
-    };
+
 
     private SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
         @Override
@@ -145,15 +149,13 @@ public class VideoPlayActivity extends BaseActivity {
             try {
                 Camera.Parameters parameters = camera.getParameters();
                 parameters.setPreviewFormat(ImageFormat.NV21); //设置数据格式
-                parameters.setPreviewSize(1280, 720);
+                parameters.setPreviewSize(1920, 1080);
                 camera.setParameters(parameters);
                 camera.setPreviewDisplay(surfaceHolder);
                 camera.startPreview();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            //编码初始化
-            encoder = new H264Encoder(width, height, framerate);
 
         }
 
